3fc6137c3caea160845aa51c72cbd76c963a858c
Joel Oliveira <joel@humanspot.com>
2016-10-06 18:21:57 +0100

    Fixes width/height of game-canvas. PS: Queria ter acesso à API da cena a Sério.

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb} );
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
